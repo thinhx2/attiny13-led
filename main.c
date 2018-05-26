4feb2601f332f371cc5f76f5db7b315fcce761ab
Himura (from netbook) <glagol15@gmail.com>
2018-05-26 18:37:45 +0300

    refactoring

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void) {
-    DDRB &= ~(1<<PB3);  // IN
-    PORTB |= (1<<PB3);  // Pull-up ON
+#define BTN_PRESSED !(PINB & (1<<PB3))
+#define LED_OFF     PORTB |= (1<<PB4)
+#define LED_ON      PORTB &= ~(1<<PB4)
 
-    DDRB |=  (1<<PB4);   // OUT
+int main(void) {
+    DDRB &= ~(1<<PB3);  // Button pin = Input
+    PORTB |= (1<<PB3);  // Pull-up = ON
+    DDRB |=  (1<<PB4);   // LED pin = Output
 	
 	while(1) {
-        if (PINB & (1<<PB3)) {  // Not pressed
-            PORTB |= (1<<PB4);  // Turn OFF
-        } else {
-		    PORTB &= ~(1<<PB4);  // Turn ON
-        }
+        if (BTN_PRESSED) LED_ON; else LED_OFF;
 	}
 	return 0;
 }
